a5aaa366957a52962f7f5c50623375545744d15e
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-03-18 23:54:20 +0530

    auth

--- imagine/app/(auth)/layout.tsx
+++ imagine/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Children } from 'react'
 
-const Layout = ({children}:{children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='auth'>{children}</main>
   )
